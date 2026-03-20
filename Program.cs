5e5d384173df2b70342951bb0b7d53be37e0c717
sabinaiurcu <sabina.iurcu@student.usv.ro>
2026-03-20 20:28:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Agenda agenda = new Agenda();
+ 
+        }
+    }
+}
---
+++ contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Program
+{
+    class Contact
+    {
+        public int Id;
+        public string Nume;
+        public string Prenume;
+        public string Telefon;
+        public string Email;
+
+        public Contact()
+        {
+            
+        }
+     
+    }
+}
